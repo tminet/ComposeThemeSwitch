e755a9574ea943563b199b9776998000bafe5c91
Thales Isidoro <tminet@live.com>
2022-11-28 09:53:21 -0300

    created an object to group app icons

---
+++ app/src/main/java/tmidev/themeswitch/util/AppIcons.kt
@@ -0,0 +1,14 @@
+package tmidev.themeswitch.util
+
+import tmidev.themeswitch.R
+
+/**
+ * Icons to be used on application.
+ */
+object AppIcons {
+    const val NavigateBack = R.drawable.ic_navigate_back
+    const val Settings = R.drawable.ic_settings
+    const val Android = R.drawable.ic_android
+    const val DarkMode = R.drawable.ic_dark_mode
+    const val LightMode = R.drawable.ic_light_mode
+}
